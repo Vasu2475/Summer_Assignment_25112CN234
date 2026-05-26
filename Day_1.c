53bc237e390c7c4f7b20ab8c45b75955cda4acb9
Vasu Pratap Singh <vasusingh2475@gmail.com>
2026-05-26 23:52:30 +0530

    Question 4 done!

--- Day_1.c
+++ Day_1.c
@@ -25,6 +25,7 @@ int main(){
 }
 */
 //Q3. Write a program to find factorial of a number
+/*
 #include <stdio.h>
 int main(){
     int fact=1, n;
@@ -35,7 +36,20 @@ int main(){
     }
     printf("The factorial of %d is %d",n,fact );
 }
-
+*/
+//Q4. Write a program to count digits in a number
+#include <stdio.h>
+int main(){
+    int count=0, n, temp;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    temp=n;
+    for(int i=0; temp>0; i++){
+        temp/=10;
+        count++;
+    }
+    printf("The number of digits in %d is %d", n, count);
+}
 
 
 
