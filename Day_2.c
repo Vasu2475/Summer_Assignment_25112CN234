44fab961b3ee06f6c07b4634f8fd43c178fe2b60
Vasu Pratap Singh <vasusingh2475@gmail.com>
2026-05-27 00:07:39 +0530

    Question 5 done!

--- Day_2.c
+++ Day_2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+//Q5. Write a program to find sum of digits of a number
+int main(){
+    int n, sum=0, temp, rem;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    temp=n;
+    for(int i=0; temp>0; i++){        
+        rem=temp%10;
+        sum+=rem;
+        temp/=10;
+    }
+    printf("The sum of digits of %d is %d", n,sum);
+}
+
+
+
+
+
 
