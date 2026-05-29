fc443f19f2c1c2fb3ec272f3bc0df0d8e2645101
Vasu Pratap Singh <vasusingh2475@gmail.com>
2026-05-29 19:45:57 +0530

    Question 13 complete

--- Day_4.c
+++ Day_4.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+//Q13. Write a program to generate fibonacci series
+int main(){
+    int n,f=0,f1=1,sum=0;
+    printf("Enter the number of terms you wanna print: ");
+    scanf("%d", &n);
+    printf("%d ",f);
+    for(int i=0; i<=n; i++){
+        printf("%d ",f1);
+        sum=f+f1;
+        f=f1;
+        f1=sum;
+    }
+    return 0;
+}
