62022876b474deb5175cabca6e29ab5c9be01c59
Vasu Pratap Singh <vasusingh2475@gmail.com>
2026-05-29 18:18:30 +0530

    Day 3 complete

--- Day_3.c
+++ Day_3.c
@@ -43,6 +43,7 @@ int main(){
 }
 */
 //Q11 Write a program to find GCD of two numbers
+/*
 int main(){
     int a,b,min,gcd=0;
     printf("Enter the 1st number: ");
@@ -65,8 +66,30 @@ int main(){
     printf("The GCD of %d and %d is %d",a,b,gcd);
     return 0;
 }
-
-
+*/
+//Q12 Write a program to find LCM of two numbers
+int main(){
+    int a,b,min,gcd=0;
+    printf("Enter the 1st number: ");
+    scanf("%d", &a);
+    printf("Enter the 2nd number: ");
+    scanf("%d", &b);
+    if(a>b){
+        min=b;
+    }
+    else{
+        min=a;
+    }
+    for(int i=1; i<=min; i++){
+        if(a%i==0 && b%i==0){
+            if(i>gcd){
+                gcd=i;
+            }
+        }
+    }
+    printf("The LCM of %d and %d is %d",a,b,((a*b)/gcd));
+    return 0;
+}
 
 
 
